1f635a5ace75d03b0f9908f55964ccaf9e694819
Rahul Kumar Maity <103112746+foxyfool@users.noreply.github.com>
2024-11-26 17:28:26 +0530

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://csv-akioz7tit-foxyfools-projects.vercel.app',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Accept'],
     credentials: true,
